e9e38f83deb5d1e9d4e2ef2f74d511222be910c4
Raghavendra Rao <raghav3276@gmail.com>
2016-10-16 09:50:07 -0700

    Modified the test file for dequeue operation
    Signed-off-by: Raghavendra Rao <raghav3276@gmail.com>

--- src/mdlist_pqueue_test.c
+++ src/mdlist_pqueue_test.c
@@ -6,10 +6,13 @@
 struct mdlist_pqueue_head myhead;
 
 struct mdlist_pqueue_node node1;
-struct mdlist_pqueue_node *node2, *node3, *node4, *node5;
+struct mdlist_pqueue_node *node2, *node3, *node4, *node5, *node6;
+struct mdlist_pqueue_node *test_node;
 
 int main()
 {
+	int i;
+
 	/* Example of node created statically */
 	mdlist_pqueue_init_node(&node1, 0x12345678);
 	mdlist_pqueue_enq(&myhead, &node1);
@@ -31,6 +34,10 @@ int main()
 	if (node5)
 		mdlist_pqueue_enq(&myhead, node5);
 
+	node6 = mdlist_pqueue_alloc_node(0x12346679);
+	if (node6)
+		mdlist_pqueue_enq(&myhead, node6);
+
 	/* Test if the list contains the nodes with the keys */
 	if (mdlist_pqueue_contains(&myhead, 0x12345678))
 		printf("Contains: 0x%x\n", 0x12345678);
@@ -49,6 +56,16 @@ int main()
 
 	if (mdlist_pqueue_contains(&myhead, 0x11111111))
 		printf("Contains: 0x%x\n", 0x11111111);
+
+	if (mdlist_pqueue_contains(&myhead, 0x12346679))
+		printf("Contains: 0x%x\n", 0x12346679);
+
+	/* Dequeue operations */
+	for (i = 0; i < 7; i++) {
+		test_node = mdlist_pqueue_deq(&myhead);
+		if (test_node)
+			printf("dequeue_node: 0x%x\n", test_node->key);
+	}
 
 	free(node2);
 	free(node3);

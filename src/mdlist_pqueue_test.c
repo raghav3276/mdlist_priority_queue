ec086426c26d67f8a2503d329b080ecc5eb8d522
Raghavendra Rao <raghav3276@gmail.com>
2016-10-16 08:38:55 -0700

    Added test cases for enqueue and contains operations
    Signed-off-by: Raghavendra Rao <raghav3276@gmail.com>

--- src/mdlist_pqueue_test.c
+++ src/mdlist_pqueue_test.c
@@ -6,7 +6,7 @@
 struct mdlist_pqueue_head myhead;
 
 struct mdlist_pqueue_node node1;
-struct mdlist_pqueue_node *node2, *node3, *node4;
+struct mdlist_pqueue_node *node2, *node3, *node4, *node5;
 
 int main()
 {
@@ -27,9 +27,33 @@ int main()
 	if (node4)
 		mdlist_pqueue_enq(&myhead, node4);
 
+	node5 = mdlist_pqueue_alloc_node(0x11345679);
+	if (node5)
+		mdlist_pqueue_enq(&myhead, node5);
+
+	/* Test if the list contains the nodes with the keys */
+	if (mdlist_pqueue_contains(&myhead, 0x12345678))
+		printf("Contains: 0x%x\n", 0x12345678);
+
+	if (mdlist_pqueue_contains(&myhead, 0x12346678))
+		printf("Contains: 0x%x\n", 0x12346678);
+
+	if (mdlist_pqueue_contains(&myhead, 0x12346679))
+		printf("Contains: 0x%x\n", 0x12346679);
+
+	if (mdlist_pqueue_contains(&myhead, 0x12345679))
+		printf("Contains: 0x%x\n", 0x12345679);
+
+	if (mdlist_pqueue_contains(&myhead, 0x11345679))
+		printf("Contains: 0x%x\n", 0x11345679);
+
+	if (mdlist_pqueue_contains(&myhead, 0x11111111))
+		printf("Contains: 0x%x\n", 0x11111111);
+
 	free(node2);
 	free(node3);
 	free(node4);
+	free(node5);
 
 	return 0;
 }
